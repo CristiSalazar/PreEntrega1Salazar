803368f84d4dcf13b1c7f794cbb97dd997ed24ad
Cristina Salazar <cristina.salazar125@gmail.com>
2023-07-25 18:23:58 -0400

    Se borran comentarios

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,28 +9,12 @@ const NavBar = () => {
             </Link>
             <nav>
                 <ul>
-                   {/* <li>
-                    <NavLink to= "/">Home</NavLink>
-                   </li>
-
-                   <li>
-                    <NavLink to="/Accion">Acción</NavLink>
-                   </li>
-
-                   <li>
-                    <NavLink to="/Fantasia">Fantasía</NavLink>
-                   </li>
-
-                   <li>
-                    <NavLink to="/Drama">Drama</NavLink>
-                   </li> */}
                    <li>
                     <NavLink to="/categoria/2">Fantasía</NavLink>
                    </li>
                    <li>
                     <NavLink to="/categoria/3">Acción</NavLink>
                    </li>
-
                 </ul>
                 <CartWidget/>
         
