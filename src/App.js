74b327cfc9a321c2e52ea3e0e9318082190238ec
Cristina Salazar <cristina.salazar125@gmail.com>
2023-08-21 18:50:49 -0400

    Checkout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Cart from "./components/Cart/Cart";
 import Productos from "./componentes/Productos";
+import Checkout from "./Checkout";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
             <Route path="/categoria/:idCategoria" element={<ItemListContainer/>}/>
             <Route path="/item/:idItem" element={<ItemDetailContainer/>}/>
             <Route path="*" element= {<h1>Página no disponible</h1>}/>
+            <Route path="/checkout" element={<Checkout/>}/>
             <Route path= "/cart" element={<Cart/>} />
           </Routes>
       </CartProvider>
